13eb2c1bf8a33eff505bd8957fdd520586c27c9b
Michal Gorny <michalgoorny@gmail.com>
2014-12-11 14:59:25 +0100

    Logout button added

--- app/src/main/java/pl/michalgorny/voteme/MainActivity.java
+++ app/src/main/java/pl/michalgorny/voteme/MainActivity.java
@@ -1,9 +1,15 @@
 package pl.michalgorny.voteme;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+    private void logoutUser() {
+        ParseUser.logOut();
+        startActivity(new Intent(this, LaunchActivity.class));
+        finish();
     }
 
 }
