30783ee45df851b23d3fffed8a0bb25f79ac9401
Michal Gorny <michalgoorny@gmail.com>
2014-12-12 11:24:31 +0100

    VoteActivity and ResultsActivity added

---
+++ app/src/main/java/pl/michalgorny/voteme/VoteActivity.java
@@ -0,0 +1,17 @@
+package pl.michalgorny.voteme;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class VoteActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+
+}
