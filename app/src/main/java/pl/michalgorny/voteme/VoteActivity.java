cf9a35834ca31071694f447b690fa976aa8a693d
Michal Gorny <michalgoorny@gmail.com>
2014-12-12 16:27:52 +0100

    RatingBars prepared for questionnaire filling

--- app/src/main/java/pl/michalgorny/voteme/VoteActivity.java
+++ app/src/main/java/pl/michalgorny/voteme/VoteActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.widget.EditText;
 import android.widget.RatingBar;
 
+import com.parse.ParseObject;
+
 import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,10 +21,34 @@ public class VoteActivity extends ActionBarActivity {
     @InjectViews({R.id.ratingBarDetails, R.id.ratingBarFeelings, R.id.ratingBarNecessity, R.id.ratingBarPlace, R.id.ratingBarTasks, R.id.ratingBarTrainer})
     List<RatingBar> mRatingsBars;
 
+    @InjectView(R.id.ratingBarDetails)
+    RatingBar mRatingBarDetails;
+
+    @InjectView(R.id.ratingBarFeelings)
+    RatingBar mRatingBarFeelings;
+
+    @InjectView(R.id.ratingBarNecessity)
+    RatingBar mRatingBarNecessity;
+
+    @InjectView(R.id.ratingBarPlace)
+    RatingBar mRatingBarPlace;
+
+    @InjectView(R.id.ratingBarTasks)
+    RatingBar mRatingBarTasks;
+
+    @InjectView(R.id.ratingBarTrainer)
+    RatingBar mRatingBarTrainer;
+
+    @InjectView(R.id.formAdditionalText)
+    EditText mAddtionalNotes;
+
+    @InjectView(R.id.formMoreTopics)
+    EditText mMoreTopics;
+
     static final ButterKnife.Action<RatingBar> DEFAULT_RATE = new ButterKnife.Action<RatingBar>() {
         @Override
         public void apply(RatingBar view, int index) {
-            view.setRating(5.0f);
+            view.setRating(0.0f);
         }
     };
 
@@ -34,6 +61,16 @@ public class VoteActivity extends ActionBarActivity {
     @OnClick(R.id.clearFormButton)
     public void setQuestionnaireToDefault(){
         ButterKnife.apply(mRatingsBars, DEFAULT_RATE);
+        mAddtionalNotes.setText("");
+        mMoreTopics.setText("");
     }
+
+    @OnClick(R.id.sendFormButton)
+    public void sendQuestionnaire(){
+        
+    }
+
+
+
 
 }
