ef5e2c4bbcbbd3b57a93236281b1298702b886b6
Michal Gorny <michalgoorny@gmail.com>
2014-12-12 15:03:30 +0100

    Defaults ratings values function added

--- app/src/main/java/pl/michalgorny/voteme/VoteActivity.java
+++ app/src/main/java/pl/michalgorny/voteme/VoteActivity.java
@@ -1,17 +1,39 @@
 package pl.michalgorny.voteme;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.InjectViews;
+import butterknife.OnClick;
 
 
 public class VoteActivity extends ActionBarActivity {
 
-    @Override
+    @InjectViews({R.id.ratingBarDetails, R.id.ratingBarFeelings, R.id.ratingBarNecessity, R.id.ratingBarPlace, R.id.ratingBarTasks, R.id.ratingBarTrainer})
+    List<RatingBar> mRatingsBars;
+
+    static final ButterKnife.Action<RatingBar> DEFAULT_RATE = new ButterKnife.Action<RatingBar>() {
+        @Override
+        public void apply(RatingBar view, int index) {
+            view.setRating(5.0f);
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.clearFormButton)
+    public void setQuestionnaireToDefault(){
+        ButterKnife.apply(mRatingsBars, DEFAULT_RATE);
     }
 
 }
