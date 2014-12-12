6e49707d0977315928f01333ed4381543033ac0f
Michal Gorny <michalgoorny@gmail.com>
2014-12-12 11:00:46 +0100

    Code refactor

--- app/src/main/java/pl/michalgorny/voteme/LaunchActivity.java
+++ app/src/main/java/pl/michalgorny/voteme/LaunchActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.ParseRole;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
@@ -19,13 +20,17 @@ public class LaunchActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         if(ParseUser.getCurrentUser() == null){
-            ParseLoginBuilder builder = new ParseLoginBuilder(this);
-            customizeLoginScreen(builder);
-            startActivityForResult(builder.build(), PARSE_LOGIN_REQUEST_CODE);
+            showLoginScreen();
         }else{
             startMainActivity();
         }
 
+    }
+
+    private void showLoginScreen() {
+        ParseLoginBuilder builder = new ParseLoginBuilder(this);
+        customizeLoginScreen(builder);
+        startActivityForResult(builder.build(), PARSE_LOGIN_REQUEST_CODE);
     }
 
     private void customizeLoginScreen(ParseLoginBuilder builder) {
@@ -35,17 +40,17 @@ public class LaunchActivity extends ActionBarActivity {
 
     }
 
+    private void startMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PARSE_LOGIN_REQUEST_CODE && resultCode == RESULT_OK){
             startMainActivity();
         }
 
-        finish();
-    }
-
-    private void startMainActivity() {
-        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
