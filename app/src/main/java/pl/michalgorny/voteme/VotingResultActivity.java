67242db4df3af1333e65b017c71f66d9a29a1658
Michal Gorny <michalgoorny@gmail.com>
2014-12-16 14:19:16 +0100

    Methods to implement added

--- app/src/main/java/pl/michalgorny/voteme/VotingResultActivity.java
+++ app/src/main/java/pl/michalgorny/voteme/VotingResultActivity.java
@@ -1,9 +1,17 @@
 package pl.michalgorny.voteme;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.HashMap;
+import java.util.List;
+
+import pl.michalgorny.voteme.model.Ratings;
 
 
 public class VotingResultActivity extends ActionBarActivity {
@@ -12,6 +20,32 @@ public class VotingResultActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting_result);
+
+        /**
+         * TODO: Get results from server. Use @ParseQuery and @Ratings classes
+         */
+
+    }
+
+    private void handleResponse(List<Ratings> ratings) {
+        HashMap<String, Float> values = new HashMap<>(ratings.size());
+
+        /**
+         * TODO: Get data from retrieved object and pass them to fragment displays it.
+         */
+
+        addFragment(values);
+    }
+
+    private Float calculateAverage(double feelings, int size) {
+        return new Float(feelings / size);
+    }
+
+    private void addFragment(HashMap<String, Float> values) {
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, RatingsFragment.newInstance(values))
+                .commit();
     }
 
 }
