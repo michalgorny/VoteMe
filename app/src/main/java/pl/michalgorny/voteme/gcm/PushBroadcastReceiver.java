f9af7762b9e5f98deac26283f55669be6b0c6792
Michal Gorny <michalgoorny@gmail.com>
2014-12-17 13:55:48 +0100

    Manifest configuration to receive push. Custom PushBroadcastReceiver created.

---
+++ app/src/main/java/pl/michalgorny/voteme/gcm/PushBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package pl.michalgorny.voteme.gcm;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+import pl.michalgorny.voteme.LaunchActivity;
+
+public class PushBroadcastReceiver extends ParsePushBroadcastReceiver {
+
+    @Override
+    protected void onPushOpen(Context context, Intent intent) {
+        context.startActivity(new Intent(context, LaunchActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+}
