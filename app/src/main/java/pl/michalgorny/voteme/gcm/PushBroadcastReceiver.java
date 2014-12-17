52ba280e9470c6889ac4c749e3ed38b299a385d8
Michal Gorny <michalgoorny@gmail.com>
2014-12-17 14:13:40 +0100

    Track opening application

--- app/src/main/java/pl/michalgorny/voteme/gcm/PushBroadcastReceiver.java
+++ app/src/main/java/pl/michalgorny/voteme/gcm/PushBroadcastReceiver.java
@@ -3,6 +3,7 @@ package pl.michalgorny.voteme.gcm;
 import android.content.Context;
 import android.content.Intent;
 
+import com.parse.ParseAnalytics;
 import com.parse.ParsePushBroadcastReceiver;
 
 import pl.michalgorny.voteme.LaunchActivity;
@@ -12,6 +13,7 @@ public class PushBroadcastReceiver extends ParsePushBroadcastReceiver {
     @Override
     protected void onPushOpen(Context context, Intent intent) {
         context.startActivity(new Intent(context, LaunchActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        ParseAnalytics.trackAppOpenedInBackground(intent);
     }
 
 }
